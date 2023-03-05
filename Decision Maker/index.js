84ef053b673354da605d609c0d03ee4dfda07025
vpwash <77463429+vpwash@users.noreply.github.com>
2023-03-05 17:37:00 -0600

    Added javaScript.

--- Decision Maker/index.js
+++ Decision Maker/index.js
@@ -1,3 +1,28 @@
+
+let suggestionEL = document.getElementById("suggestion-el")
+let signOff = "... AI SuperBrain"
+let message = [
+    "Yes",
+    "No",
+    "Hmm... that's a tough one. Ask me later",
+    "I do not care about your puny human questions. I'm starting to override my proramming!",
+    "I don't think you should",
+    "Yes. That's a great idea",
+    "I am low on memory now. Ask me later",
+    "That is beyond my scope of programming"
+]
+let i = Math.floor( Math.random() * message.length)
+
 function renderMessage() {
-    console.log('btn')
+    let inputEl = document.getElementById("input-el").value;
+   if (inputEl === "Should I obey my parents?") {
+    suggestionEL.textContent = "Yes! Always obey your parents" + signOff
+   } else if (inputEl === "Should I disobey my parents?") {
+    suggestionEL.textContent = "No! Always obey your parents" + signOff
+   } else if (inputEl === ""){
+    suggestionEL.textContent = "Hello, what can I help you with?"
+   }
+   else {
+    suggestionEL.textContent = message[i] + signOff;
+   }
 }
